a5176ffd74330c7f77b613cced2e44affb5e5a78
Amar Rianto <amarrianto6969@gmail.com>
2021-06-14 00:58:00 +0700

    FORM LOGIN DONE

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,45 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-container>
+      <v-card tile elevation="1" class="mt-5">
+        <v-card-tittle>
+          <h1>Login</h1>
+          <v-card-text>
+            <v-form>
+              <v-text-field
+                label="Username"
+                prepend-icon="mdi-account-circle"
+              />
+              <v-text-field
+                label="Password"
+                prepend-icon="mdi-lock"
+                :type="showPassword ? 'text' : 'password'"
+                :append-icon="showPassword ? 'mdi-eye' :'mdi-eye-off'" @click:append="handleClick"
+              />
+            </v-form>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn color="success">Login</v-btn>
+            <v-btn color="info">Register</v-btn>
+          </v-card-actions>
+        </v-card-tittle>
+      </v-card>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  data(){
+    return{
+      showPassword:false
+    }
+  },
+  methods:{
+    handleClick(){
+      this.showPassword = !this.showPassword
+    }
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
