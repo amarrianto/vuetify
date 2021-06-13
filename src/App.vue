29b0ed373e547820e56175dc0e31484063dd9fc6
Amar Rianto <amarrianto6969@gmail.com>
2021-06-14 01:20:59 +0700

    Mencetak Data Dummy

--- src/App.vue
+++ src/App.vue
@@ -4,25 +4,29 @@
       <v-card tile elevation="1" class="mt-5">
         <v-card-tittle>
           <h1>Login</h1>
+        </v-card-tittle>
+        <v-form @submit.prevent="handleSubmit">
           <v-card-text>
-            <v-form>
-              <v-text-field
-                label="Username"
-                prepend-icon="mdi-account-circle"
-              />
-              <v-text-field
-                label="Password"
-                prepend-icon="mdi-lock"
-                :type="showPassword ? 'text' : 'password'"
-                :append-icon="showPassword ? 'mdi-eye' :'mdi-eye-off'" @click:append="handleClick"
-              />
-            </v-form>
+            <v-text-field
+              label="Email"
+              prepend-icon="mdi-account-circle"
+              v-model="user.email"
+            />
+            <v-text-field
+              label="Password"
+              prepend-icon="mdi-lock"
+              :type="showPassword ? 'text' : 'password'"
+              :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+              @click:append="handleClick"
+              v-model="user.password"
+            />
           </v-card-text>
+          <v-divider></v-divider>
           <v-card-actions>
-            <v-btn color="success">Login</v-btn>
+            <v-btn color="success" type="submit">Login</v-btn>
             <v-btn color="info">Register</v-btn>
           </v-card-actions>
-        </v-card-tittle>
+        </v-form>
       </v-card>
     </v-container>
   </v-app>
@@ -31,15 +35,22 @@
 <script>
 export default {
   name: "App",
-  data(){
-    return{
-      showPassword:false
-    }
+  data() {
+    return {
+      showPassword: false,
+      user: {
+        email: "",
+        password: "",
+      },
+    };
   },
-  methods:{
-    handleClick(){
-      this.showPassword = !this.showPassword
-    }
-  }
+  methods: {
+    handleClick() {
+      this.showPassword = !this.showPassword;
+    },
+    handleSubmit() {
+      console.log(this.user);
+    },
+  },
 };
 </script>
